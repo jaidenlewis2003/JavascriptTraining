859fde0ba1e151a9db7da28cb7735b11b3c11e74
Jaiden Lewis <jaiden0706@gmail.com>
2024-03-30 03:22:26 -0500

    Understanding Functions v.1

--- course/03-07/script.js
+++ course/03-07/script.js
@@ -1,3 +1,18 @@
 var pens; 
 pens = ["orange", "blue", "purple", "green"];
 console.log(pens);
+
+// var fourthpen = pens[3];
+
+// console.log(fourthpen);
+
+
+pens.unshift("Black", "Todo");
+
+
+
+var newPens = pens.slice();
+console.log("New pens: ", newPens);
+
+ var arraryString = pens.join("  "); 
+ console.log("String from array: ", arraryString);
